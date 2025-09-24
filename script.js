e0624bb9c46d01a32eadbdff9ed718aa8cf95f9a
Gustavo Galvão <gustavojezler@gmail.com>
2025-09-24 20:03:19 -0300

    update 1.2

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ function enviarWhats(event) {
     console.log(url)
 
 }
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  let requestId = null;
+
+  card.addEventListener('mousemove', e => {
+    if (requestId) return; // evita múltiplas chamadas simultâneas
+
+    requestId = requestAnimationFrame(() => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      const centerX = rect.width / 2;
+      const centerY = rect.height / 2;
+
+      const rotateX = ((y - centerY) / centerY) * -50;
+      const rotateY = ((x - centerX) / centerX) * 50;
+
+      card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+      requestId = null;
+    });
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  });
+});
